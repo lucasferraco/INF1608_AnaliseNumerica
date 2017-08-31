90142186d267faeef5acb2be48484c236884a59c
lucasferraco <lucasferracof@gmail.com>
2017-08-31 11:26:23 -0300

    LAB2: updated delivery folder

--- LAB2/main.c
+++ LAB2/main.c
@@ -15,38 +15,38 @@ double bisec_teste(double x) {
 }
 
 double pf_teste(double x) {
-	return (cos(x)+2)/3;
+	return (cos(x)+2.0)/3.0;
 }
 
 int main(int argc, const char * argv[]) {
-	double a = 0.25, b = 0.5, p = pow(10, -6), x0 = 1.0;
+	double a = 0.25, b = 0.5, p = 6, x0 = 1.0;
 	double raio = 1.0, volume = 4.0/3.0*M_PI / 2.0;
 	
 	// Teste bissecao
 	printf("Teste bissecao\n");
 	printf("funcao = pow(x, 3) - x*sin(2*x) + 0.2\n");
-	printf("limites: inf = %.10g \tsup = %.10g\n", a, b);
-	printf("precisao = %.10g \n", p);
-	printf("bissecao = %.10g \n", bissecao(a, b, p, bisec_teste));
+	printf("limites: inf = %.7g \tsup = %.10g\n", a, b);
+	printf("precisao = %.7g \n", p);
+	printf("bissecao = %.7g \n", bissecao(a, b, p, bisec_teste));
 	printf("\n");
 	
 	// Teste altura_agua
 	printf("Teste altura_agua\n");
-	printf("raio = %.10g \tvolume = %.10g\n", raio, volume);
-	printf("altura_agua = %.10g \n", altura_agua(raio, volume));
+	printf("raio = %.7g \tvolume = %.7g\n", raio, volume);
+	printf("altura_agua = %.7g \n", altura_agua(raio, volume));
 	printf("\n");
 	
 	// Teste pontofixo
 	printf("Teste pontofixo\n");
 	printf("funcao = (cos(x)+2)/3\n");
-	printf("x0 = %.10g\n", x0);
-	printf("epsilon = %.10g \n", p);
-	printf("pontofixo = %.10g \n", pontofixo(x0, p, pf_teste));
+	printf("x0 = %.7g\n", x0);
+	printf("epsilon = %.7g \n", p);
+	printf("pontofixo = %.7g \n", pontofixo(x0, pow(10, -p), pf_teste));
 	printf("\n");
 	
 	// Teste fraiz
 	printf("Teste fraiz\n");
-	printf("fraiz(3) = %.10g \n", fraiz(3));
+	printf("fraiz(3) = %.7g \n", fraiz(3));
 	printf("\n");
 	
     return 0;
